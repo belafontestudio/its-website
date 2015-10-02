9c654615b1a48e879aca9fd0ce7015af8c76784c
Nicolò Ottenga <otteng.nic@gmail.com>
2015-10-02 15:10:53 +0200

    major commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,4 +3,33 @@
 $(document).ready(function() {
   $(".drawer").drawer();
 
+  $(".menu-web").sticky({topSpacing:0});
+
+
+  $('.cosa').click(function(e){
+    e.preventDefault();
+    $(window).scrollTo($('#cosa-sono'), 800);
+  });
+  $('.aree').click(function(e){
+    e.preventDefault();
+    $(window).scrollTo($('#aree-formative'), 800);
+  });
+  $('.inp').click(function(e){
+    e.preventDefault();
+    $(window).scrollTo($('#in-piemonte'), 800);
+  });
+  $('.offerta').click(function(e){
+    e.preventDefault();
+    $(window).scrollTo($('#cosa-sono'), 800);
+  });
+  $('.contatti').click(function(e){
+    e.preventDefault();
+    $(window).scrollTo($('#footer'), 800);
+  });
+
+
+
+
+
+
 });
